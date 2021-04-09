1cbc16cbbaa2e08bf58b2b483ba6895445c33b1c
Francisco Alejandro Cifuentes Romero <79933248+Francisco-Cifuentes@users.noreply.github.com>
2021-04-09 14:24:58 -0500

    Add files via upload

---
+++ Calificaciones/Calificaciones/Modelo/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calificaciones.Modelo
+{
+    public partial class Estudiante
+    {
+        public int Cod { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public double Prome { get; set; }
+        public string Aprob { get; set; }
+    }
+}
---
+++ Calificaciones/Calificaciones/Modelo/estudiantesContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Calificaciones.Modelo
+{
+    public partial class estudiantesContext : DbContext
+    {
+        public virtual DbSet<Estudiante> Estudiante { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("Server=localhost; userid=root; password=12345678; Database=estudiantes");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudiante>(entity =>
+            {
+                entity.HasKey(e => e.Cod);
+
+                entity.ToTable("estudiante");
+
+                entity.Property(e => e.Cod)
+                    .HasColumnName("cod")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Aprob)
+                    .IsRequired()
+                    .HasColumnName("aprob")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Correo)
+                    .IsRequired()
+                    .HasColumnName("correo")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Nota1).HasColumnName("nota1");
+
+                entity.Property(e => e.Nota2).HasColumnName("nota2");
+
+                entity.Property(e => e.Nota3).HasColumnName("nota3");
+
+                entity.Property(e => e.Prome).HasColumnName("prome");
+            });
+        }
+    }
+}
